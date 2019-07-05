aa7a59e879ff378283642063cfb974b78800a678
Pranav <pnarang1130@gmail.com>
2019-07-04 21:21:58 -0600

    1. Downloaded jq using brew 2. Stored raw response from curl in a variable 3. Passed raw response to jq by piping 4. Parsed JSON response to find URL 5. Stored URL in a variable 6. Updated TODO's

--- Script.sh
+++ Script.sh
@@ -5,16 +5,20 @@ API_KEY=12947236-10bedb42dfb9be1de81626120
 
 URL=https://pixabay.com/api/
 
-API_CALL="$URL?key=$API_KEY+&q=nature+wallpaper&min_width=2880&min_height=1800&order=latest&per_page=3&editors_choice=true"
+API_CALL="$URL?key=$API_KEY&q=nature+wallpaper&min_width=2880&min_height=1800&order=latest&per_page=3&editors_choice=true"
 
-echo $API_CALL
+# echo $API_CALL
 
 # https://pixabay.com/api/?key=12947236-10bedb42dfb9be1de81626120&q=yellow+flowers&image_type=photo
 
+rawResponse=$(curl -s $API_CALL)
+
+url=$(echo $rawResponse | jq .hits[0].largeImageURL)
+
+echo $url
+
+
 # TODO:
-# 1. Download JS
-# 2. Parse response taking .hits[0].largeImageURL
-# 3. save image URL as a variable
 # 4. download image to local location in TMP folder using curl
 # 5. change wallpaper using:
 # osascript -e ‘tell application “Finder” to set desktop picture to POSIX file “link/to/download.jpg”’
