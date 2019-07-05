ae8ca7c86a18685a9e4711a72ea533909ffcba15
Pranav <pnarang1130@gmail.com>
2019-07-04 21:56:32 -0600

    Successfully downloaded image using curl and modifying URL string

--- Script.sh
+++ Script.sh
@@ -7,19 +7,23 @@ URL=https://pixabay.com/api/
 
 API_CALL="$URL?key=$API_KEY&q=nature+wallpaper&min_width=2880&min_height=1800&order=latest&per_page=3&editors_choice=true"
 
-# echo $API_CALL
-
 # https://pixabay.com/api/?key=12947236-10bedb42dfb9be1de81626120&q=yellow+flowers&image_type=photo
 
 rawResponse=$(curl -s $API_CALL)
 
 url=$(echo $rawResponse | jq .hits[0].largeImageURL)
+#Remove first and last " from URL
+url="${url%\"}"
+url="${url#\"}"
 
-echo $url
+#Download URL
+curl ${url} > image.png
 
+cmd="tell application \"Finder\" to set desktop picture to \"/Users/Pranav/Documents/Code/WallPaperScript/image.png\" as POSIX file"
+osascript -e "$cmd"
 
 # TODO:
-# 4. download image to local location in TMP folder using curl
+
 # 5. change wallpaper using:
 # osascript -e ‘tell application “Finder” to set desktop picture to POSIX file “link/to/download.jpg”’
 
